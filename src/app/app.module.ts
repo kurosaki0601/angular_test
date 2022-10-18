7ecd0c750dee9525e30ff2cece8762834757c846
Dinh Manh <manhtd20@gmail.com>
2022-10-18 11:11:34 +0700

    create cpn footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
